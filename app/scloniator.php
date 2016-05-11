aa53e50221594e70e5287e2f0be3d217a74e1de3
Aleksandr <map.m@ua.fm>
2016-05-11 12:26:02 +0300

    analiz siblings words

--- app/scloniator.php
+++ app/scloniator.php
@@ -17,7 +17,7 @@ if(isset($_POST['words'])){
   }
 
   foreach($words_arr as $word){
-    $words_res[] = wordPrepare($word);
+    $words_res = array_merge($words_res, wordPrepare($word));
   }
 }
 
@@ -35,16 +35,15 @@ echo json_encode($array);
 //-----------------------------------------------------------------
 
 function wordPrepare($word){
-  $result = '';
-  $vowels = ['а', 'о', 'э', 'и', 'у', 'ы', 'е', 'ё', 'ю', 'я'];
+  $result = [];
+  $result[] = $word;
+  $vowels = ['а', 'о', 'э', 'и', 'у', 'ы', 'е', 'ё', 'ю', 'я', 'й'];
   $last_char = mb_substr($word, -1, NULL, 'UTF-8');
 
   if( mb_strlen($word, 'UTF-8')===1 && !in_array($word, $vowels) ) {
-    $result = str_repeat($word, 2);
+    $result[] = str_repeat($word, 2);
   }else if( !in_array($last_char, $vowels) ) {
-    $result = $word . 'а';
-  }else{
-    $result = $word;
+    $result[] = $word . 'а';
   }
   return $result;
 }
